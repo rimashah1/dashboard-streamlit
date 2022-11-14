078f0d720debac36026b9b5bae19325043ccab8c
rimashah1 <rima.shah.1@stonybrook.edu>
2022-11-14 18:53:21 -0500

    Updates

--- heartfailure.py
+++ heartfailure.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+# Load Data
 DATA_URL = ('https://raw.githubusercontent.com/sauravmishra1710/Heart-Failure-Condition-And-Survival-Analysis/master/Data/heart_failure_clinical_records_dataset.csv')
 
 def load_data(nrows):
@@ -13,21 +14,28 @@ def load_data(nrows):
 # Load 10,000 rows of data into the dataframe.
 data = load_data(10000)
 
+
+# Add title
 st.title('Heart Failure Data')
 
+# Display raw data in df
 st.subheader('Raw data')
 st.write(data)
 
+
+# Display data that we will use in df
 st.subheader('Pertinent Data')
 st.text('Below is the data used for the visualizations')
 age_hbp = data[['age', 'platelets']]
 st.write(age_hbp)
 
-st.subheader('Histogram')
+
+# Visualize data with area chart
+st.subheader('Area Chart')
 st.text('Below is the code to make an area chart')
 
 code = '''
-        age_hbp = data[['age', 'platelets']]
+        age_hbp = data[['age', 'platelets']
         st.area_chart(data=age_hbp, x='age', y='platelets')
         '''
 st.code(code, language='python')
@@ -35,6 +43,7 @@ st.code(code, language='python')
 st.area_chart(data=age_hbp, x='age', y='platelets')
 
 
+# Visualize data with line chart
 st.subheader('Line Plot')
 st.text('Below is the data visualized in a line plot')
 age_hbp = data[['age', 'platelets']]
